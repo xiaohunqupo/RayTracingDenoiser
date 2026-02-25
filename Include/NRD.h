927e4953f9e52b0686976ebba76ffe0142bfc8a6
dzhdan <dzhdan@nvidia.com>
2026-02-25 19:08:13 +0800

    Updated date

--- Include/NRD.h
+++ Include/NRD.h
@@ -30,7 +30,7 @@ CREDITS:
 #define NRD_VERSION_MAJOR 4
 #define NRD_VERSION_MINOR 17
 #define NRD_VERSION_BUILD 1
-#define NRD_VERSION_DATE "9 February 2026"
+#define NRD_VERSION_DATE "25 February 2026"
 
 #if defined(_WIN32)
     #define NRD_CALL __stdcall
